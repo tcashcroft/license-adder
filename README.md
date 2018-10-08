507daabe78fd05444550336da52120d2b1885335
Tyler Ashcroft <tyler_ashcroft@byu.edu>
2018-10-08 16:57:38 -0600

    Added empyty Readme, reworked project into main.

---
+++ README.md
@@ -0,0 +1,16 @@
+Skills Evaluation:
+
+Write code, in the language of your choice, that using the github API, goes through all the public and private github repos in a github organization and checks if they have a license.  (You can create a free github.com organization to test with here.)
+Write code that if the license is missing, opens a pull request which adds a license. (More info about licenses can be found here: https://choosealicense.com/)
+Save your code (finished or not) into a public github repo.
+Document any assumptions you made, instructions on how to run your code and anything else you want me to know using a README.md file in the root of the directory tree in the repo.
+Reply to this email giving me the link to the repo with your code at or before Wednesday, October 10, 2018. If you don't finish all of the requirements on time please send the link by the deadline anyway.
+We give preference to submissions in go/python/kotlin​/typescript/nodejs but outstanding submissions in any language with a strong willingness to learn new technologies will be seriously considered.
+
+ 
+
+If you haven't dealt with web service APIs, github or AWS before there is publicly available and easily found (via google) documentation on each technology/product required for the evaluation.
+
+ 
+
+Best regards and happy coding!
--- src/main.py
+++ src/main.py
@@ -1,36 +1,136 @@
-#!/bin/python
 
 import argparse
+from github import Github
+from github import GithubException
+
 
 def get_argument_parser():
     """
-
-    :return:
+    Prepares the argument parser. Command line arguments can be added in this method.
+    :return: ArgumentParser
     """
     parser = argparse.ArgumentParser(
         prog='license-adder',
-        description='Adds license files to repositories in the given GitHub project.'
+        description='Adds licenses to repositories that do not have licenses in a given organization in Github'
     )
     parser.add_argument(
-        '-h',
-        '--help',
-        action='store_true',
-        default=False,
+        '-o',
+        '--organization',
+        action='store',
+        dest='organization'
+    )
+    parser.add_argument(
+        '-u',
+        '--username',
+        action='store',
+        dest='username',
+        default=None
     )
     parser.add_argument(
         '-p',
-        '--project',
+        '--password',
         action='store',
-        dest='project',
+        dest='password',
+        default=None
     )
+    parser.add_argument(
+        '-c',
+        '--credentials',
+        dest='credentials',
+        default=None
+    )
+    parser.add_argument(
+        '-l',
+        '--license',
+        action='store',
+        dest='license',
+        default='LICENSE'
+    )
+    parser.add_argument(
+        '-b'
+        '--base',
+        action='store',
+        dest='base',
+        default='master'
+    )
+    parser.add_argument(
+        '-n',
+        '--new-branch',
+        action='store',
+        dest='head',
+        default='license'
+    )
+    # TODO: Allow for a credentials file
 
     return parser
 
+
+def get_license(path):
+    with open(path) as license_file:
+        return license_file.read().replace('\n', '')
+
+
+def get_credentials(path):
+    with open(path) as credentials_file:
+        for index, value in enumerate(credentials_file.readlines()):
+            if index == 0:
+                username = value.strip()
+            if index == 1:
+                password = value.strip()
+        return username, password
+
+
+def add_license(repo, license_contents, base, head):
+    base_branch = repo.get_branch(base)
+    license_branch = repo.create_git_ref('refs/heads/' + head, sha=base_branch.commit.sha)
+    repo.create_file("/LICENSE", "Adding License", license_contents, branch=head)
+    repo.create_pull(title="License Added", body="Added a license to this repository.", base=base, head=head)
+
+
 def main():
-    parser = get_argument_parser()
-    args = parser.parse_args()
+    argparser = get_argument_parser()
+    args = argparser.parse_args()
 
-    project_name = args.project
+    credentials = args.credentials
+    username = args.username
+    password = args.password
+    base = args.base
+    head = args.head
+    license_path = args.license
 
-if __name__  == "__main__":
+    if credentials is None:
+        if username is None or password is None:
+            # username and password must both be provided if credentials is None
+            pass
+    else:
+        username, password = get_credentials(credentials)
+        if username is None or not username or password is None or not password:
+            # username and password must both be provided in credentials file
+            pass
+
+    organization_name = args.organization
+    if organization_name is None or not organization_name:
+        # organization name must be provided
+        pass
+
+    if license_path is None or not license_path:
+        # license path must be provided
+        pass
+
+    license_content = get_license(license_path)
+    github_client = Github(username, password)
+    organization = github_client.get_organization(organization_name)
+
+    repos_without_licenses = []
+    for repo in organization.get_repos():
+        try:
+            repo.get_license()
+        except GithubException:
+            repos_without_licenses.append(repo)
+
+    for repo in repos_without_licenses:
+        add_license(repo, license_content, base, head)
+
+
+if __name__ == "__main__":
     main()
